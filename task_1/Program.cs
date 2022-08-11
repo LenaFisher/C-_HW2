d036d10d3dcc28dea106462cd01efa6f7ef5574a
«Elena <snogotok@gmail.com>
2022-08-11 13:34:12 +0300

    commit hw2

---
+++ task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 1000 & number > 99)
+// {
+//     int temp1 = (number / 100) * 100;
+//     int temp2 = (number - temp1) / 10;
+//     Console.WriteLine(temp2);
+//     break;
+// }
+
+
+
+
+
+
+// Console.WriteLine("Вы ввели не трехзначное число");
+
+if (number < 1000 & number > 99)
+{
+    int temp1 = (number / 100) * 100;
+    int temp2 = (number - temp1) / 10;
+    Console.WriteLine(temp2);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+
+
+
+
+
+
+
+
---
+++ task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length < 3)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (i == 2)
+        {
+            Console.WriteLine(number[i]);
+        }
+
+    }
+}
+
+
